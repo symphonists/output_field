8367e3c7ad040a59830ae7206aee7ba79cf011ee
Nils Hörrmann <post@nilshoerrmann.de>
2012-02-27 11:16:34 +0100

    Symphony 2.3 compatibility

--- extension.driver.php
+++ extension.driver.php
@@ -1,33 +1,17 @@
 <?php
 
+	/**
+	 * @package output_field
+	 */
+	/**
+	 * Output Field Extension
+	 */
 	Class extension_output_field extends Extension {
 
 		/**
-		 * Extension information
+		 * @see http://symphony-cms.com/learn/api/2.3/toolkit/extension/#install
 		 */
-		public function about() {
-			return array(
-				'name' => 'Field: Output',
-				'version' => '1.2.1',
-				'release-date' => '2011-12-13',
-				'author' => array(
-					'name' => 'Nils Hörrmann',
-					'website' => 'http://nilshoerrmann.de',
-					'email' => 'post@nilshoerrmann.de'
-				),
-				'description' => 'An input field with advanced data source output for mail addresses and URIs.'
-			);
-		}
-
-		public function uninstall() {
-		
-			// Drop database table
-			Symphony::Database()->query("DROP TABLE `tbl_fields_output`");
-		}
-
 		public function install() {
-		
-			// Create database table and fields.
 			return Symphony::Database()->query(
 				"CREATE TABLE `tbl_fields_output` (
 					`id` int(11) unsigned NOT NULL auto_increment,
@@ -37,6 +21,13 @@
 					KEY `field_id` (`field_id`)
 				)"
 			);
+		}
+
+		/**
+		 * @see http://symphony-cms.com/learn/api/2.3/toolkit/extension/#uninstall
+		 */
+		public function uninstall() {
+			Symphony::Database()->query("DROP TABLE `tbl_fields_output`");
 		}
 
 	}
--- fields/field.output.php
+++ fields/field.output.php
@@ -1,19 +1,33 @@
 <?php
 
+	/**
+	 * @package output_field
+	 */
+	/**
+	 * Advanced data source output for input fields.
+	 */
 	if (!defined('__IN_SYMPHONY__')) die('<h2>Symphony Error</h2><p>You cannot directly access this file</p>');
 
 	require_once(TOOLKIT . '/fields/field.input.php');
 
 	class FieldOutput extends FieldInput {
 
-		public function __construct(&$parent){
-			parent::__construct($parent);
+		public function __construct(){
+			parent::__construct();
 			$this->_name = __('Text Output');
 			$this->_required = true;
 			
 			$this->set('required', 'no');
 		}
 
+
+	/*-------------------------------------------------------------------------
+		Output:
+	-------------------------------------------------------------------------*/
+
+		/**
+		 * @see http://symphony-cms.com/learn/api/2.3/toolkit/field/#appendFormattedElement
+		 */
 		function appendFormattedElement(&$wrapper, $data, $encode=false){
 			
 			include(TOOLKIT . '/util.validators.php');
@@ -23,6 +37,7 @@
 			
 			$value = General::sanitize($value);
 
+			// E-Mail
 			if($this->get('validator') == $validators['email']) {
 				$mail = explode('@', $value);
 				$attributes = array(
@@ -31,9 +46,13 @@
 					'hash' => md5(strtolower($value))
 				);
  			}
+ 			
+ 			// URI
 			elseif($this->get('validator') == $validators['URI']) {
 				$attributes = parse_url($value);
 			}
+			
+			// Other input
  			else {
 				$attributes = array(
 					'handle' => $data['handle']
